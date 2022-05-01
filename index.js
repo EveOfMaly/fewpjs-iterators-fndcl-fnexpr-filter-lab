552078363f0456f496b8b66a9209b002bf8c30dc
EveOfMaly <john.a.kyeremeh@gmail.com>
2022-04-30 20:52:36 -0400

    commit tree 4ec1b88dbcdc6932e3a12316d00e3b82b4fcfcbe

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution here
+
+
+function findMatching(source , search) {
+   return source.filter(driver => 
+         driver.toLowerCase() === search.toLowerCase()
+    )
+}
+
+
+
+function fuzzyMatch(source, search) {
+    let index = search.length
+    return source.filter(driver => 
+      driver.substr(0, index) === search
+    )
+}
+
+
+ function matchName(source, search) {
+     return source.filter(record => record.name === search)
+ }
+
+
+
+
+
+
+ 
